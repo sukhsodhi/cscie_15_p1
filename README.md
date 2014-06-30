66fcca8b4a9ad9bb13c91d0a0243aa8e5dc1d33d
Sukh Sodhi <sukhm@hotmail.com>
2014-06-29 22:02:31 -0600

    Addign Index.php and images

---
+++ README.md
@@ -0,0 +1,8 @@
+## Readme for the First Assignment - CSCIE-15
+
+# Assignment -1
+
+The link for Padoba box is http://sukhsodhi-p1.gopagoda.com
+
+This project is part of the CSCIE015 summer course at Harvard University.
+The P1 uses the bootstrap framework. The bootstrap frame is reterived from the CDN.
---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+  <title>First Assignment for CSCIE-15 class</title>
+
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</head>
+<body role="document"  >
+    <div class="container theme-showcase" role="main">
+  	
+  	<div class="row" align="center">
+  		<h1>Sukh Sodhi - Assignment 1<h1>
+        <div class="col-sm-12" >
+		  	<img src="images/sukhsodhi.jpg" />
+		  	<p/>
+		 </div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">About me</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>I work as a Technology Manager with an e-commerce company called <a href="http://www.crucial.com">Crucial.com</a> (it sells Memory and SSD products).
+  					<br/>I am working towards a masters in Computer Science from the Extension School program.
+  					</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+
+    <div class="row">
+    	<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">Why are you taking this course?</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>I am taking this course to learn php. It's one of the web application technologies I have not worked with.
+            		</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+
+    <div class="row">
+    	<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">What previous experience do you have with HTML/CSS?</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>I am very comfortable with HTML/CSS development<br/>
+            			However at work we have a different team that does the Front-end work.
+            		</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+
+    <div class="row">
+    	<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">What previous experience do you have with programming?</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>About 15 years experience working on different technology stack like
+            			<ul>
+            				<li>C# , ASP.NET </li>
+            				<li>Java application</li>
+            				<li>ASP, Visual basic (pre-2000)</li>
+            				<li>Mobile programming in Objective C, Android, Windows Phone</li>
+            			</ul>
+            		</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+
+    <div class="row">
+    	<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">Have you worked with PHP before? If so, to what extent?</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>Very little, I helped someone fix a php site once.
+            		</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+
+    <div class="row">
+    	<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">Are you working on a Mac, PC, or some other platform?</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>I am a PC guys using Microsoft technology.<br/>
+            			For this course I am using a Mac.
+            		</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+
+    <div class="row">
+    	<div class="col-sm-2">&nbsp;
+		</div>
+        <div class="col-sm-8">
+			<div class="panel panel-primary">
+        		<div class="panel-heading">              
+        			<h3 class="panel-title">Anything else we should know?</h3>
+        		</div>
+        		<div class="panel-body">
+            		<p>Very little, I helped someone fix a php site once.
+            		</p>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-sm-2">&nbsp;
+		</div>
+    </div>
+</div>
+
+</body>
+</html>
